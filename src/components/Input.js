0c653e2478a5e423fdf12d3835b47e249d187b16
MICHAEL <MJENNINGS6@gmail.com>
2020-11-19 12:52:48 -0500

    completed with notes

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,19 +1,52 @@
-import React, { Component } from 'react'
+// import React, { Component } from 'react'
 
-class Input extends Component {
-  constructor(props) {
-    super(props)
-  }
+// class Input extends Component {
+//   constructor(props) {
+//     super(props)
+//   }
 
-  render() {
-    return (
-      <div>
-        <label>Input Task: </label>
-        <input type="text" name="task" />
-        <button>Add</button>
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div>
+//         <label>Input Task: </label>
+//         <input type="text" name="task" />
+//         <button>Add</button>
+//       </div>
+//     )
+//   }
+// }
 
-export default Input
+// export default Input
+
+// import React from "react";
+
+// const Input = (props) => {
+//   return (
+//     <div>
+//       <label>Input Task: </label>
+//       <input type="text" name="task" />
+//       <button onClick={props.addTask}>Add</button>
+//     </div>
+//   );
+// };
+
+// export default Input;
+
+import React from "react";
+
+const Input = (props) => {
+  return (
+    <div>
+      <label>Input Task: </label>
+      <input
+        type="text"
+        name="task"
+        value={props.value}
+        onChange={props.handleChange}
+      />
+      <button onClick={props.addTask}>Add</button>
+    </div>
+  );
+};
+
+export default Input;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,23 +1,65 @@
-import React, { Component } from 'react'
-import Tasks from './Tasks'
-import Input from './Input'
+// import React, { Component } from 'react'
+// import Tasks from './Tasks'
+// import Input from './Input'
 
-class Todolist extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      tasks: []
-    }
-  }
+// class Todolist extends Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       tasks: []
+//     }
+//   }
 
-  render() {
-    return (
-      <div className="list">
-        <Input />
-        <Tasks tasks={this.state.tasks} />
-      </div>
-    )
-  }
-}
+//   render() {
+//     return (
+//       <div className="list">
+//         <Input />
+//         <Tasks tasks={this.state.tasks} />
+//       </div>
+//     )
+//   }
+// }
 
-export default Todolist
+// export default Todolist
+
+import React, { useState } from "react";
+import Tasks from "./Tasks";
+import Input from "./Input";
+
+const Todolist = () => {
+  const [tasks, manageTasks] = useState([
+    "Wow Much Do",
+    "Much Do Wow",
+    "Walk the doge",
+    "Pet the doge",
+  ]);
+  const [task, setTask] = useState("");
+
+  const addTask = () => {
+    let myNewList = [...tasks, task];
+    manageTasks(myNewList);
+    setTask("");
+  };
+
+  const handleChange = (event) => {
+    setTask(event.target.value);
+  };
+
+  const removeTask = (index) => {
+    const taskArr = [...tasks];
+    taskArr.splice(index, 1);
+    console.log(tasks);
+    manageTasks(taskArr);
+  };
+
+  console.log(tasks);
+
+  return (
+    <div className="list">
+      <Input handleChange={handleChange} addTask={addTask} value={task} />
+      <Tasks tasks={tasks} removeTask={removeTask} />
+    </div>
+  );
+};
+
+export default Todolist;
